133909ab035828407eae2f11b95f0eccf1e74961
NOE <ncaala@miumg.edu.gt>
2023-11-13 15:11:03 -0600

    Cambios

--- src/src/modulo_auth/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-app_name = 'authenticate'
-
-urlpatterns = [
-    path('login/', LoginFormView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
-]
